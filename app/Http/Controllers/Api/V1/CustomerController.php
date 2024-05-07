45c8af5c868fe0952d02f751065d493b8292c3b7
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-05-07 14:42:23 +0330

    feat: Include customers `related data` in index.

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -19,15 +19,16 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
         $filter = new CustomersFilter();
-        $queryItems = $filter->transform($request);    // (['column', 'operator', 'value'])
+        $filterItems = $filter->transform($request);    // (['column', 'operator', 'value'])
 
-        if (count($queryItems) == 0) {
-            return new CustomerCollection(Customer::paginate());
-        } else {
-            $customers = Customer::where($queryItems)->paginate();
+        $includeInvoices = $request->query('includeInvoices');
+        $customers = Customer::where($filterItems);
 
-            return new CustomerCollection($customers->appends($request->query()));
+        if ($includeInvoices) {
+            $customers = $customers->with('invoices');
         }
+
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
     }
 
     /**
